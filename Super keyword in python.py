01ddc0209826dd720f42d237421425fccdc00bbb
Deepikarukmani <140103694+Deepikarukmani@users.noreply.github.com>
2023-09-01 15:06:57 +0530

    Update Super keyword in python.py

--- Super keyword in python.py
+++ Super keyword in python.py
@@ -1,5 +1,23 @@
+class a():
+    def __init__(self):
+        print("A")
+    def display(self):
+        print("your in class a")
 
+class b():
+    def __init__(self):
+        super().__init__()
+        print("B")
+    def display(self):
+        print("your in class b")
 
+class c(a,b):
+    def __init__(self):
+        super().__init__()
+        print("C")
+    def display(self):
+        print("your in class c")
 
-
-
+ob1=c()
+ob2=a()
+ob3=b()
